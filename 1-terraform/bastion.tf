819ed3a1273c1c36e41b8a0c54e3ed80ee6cac4f
Jess Frazelle <acidburn@microsoft.com>
2018-10-12 20:19:51 -0400

    allow port 80
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- 1-terraform/bastion.tf
+++ 1-terraform/bastion.tf
@@ -31,6 +31,19 @@ resource "azurerm_network_security_group" "bastion_nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "allow_HTTP_in_all"
+    description                = "Allow HTTP in from all locations"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags {
     app = "mesos-cluster"
   }
