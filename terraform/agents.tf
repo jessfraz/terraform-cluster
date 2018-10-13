23f007a912aff34be5b730889c1035d3063cdacd
Jess Frazelle <acidburn@microsoft.com>
2018-10-13 01:02:56 -0400

    use count
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- terraform/agents.tf
+++ terraform/agents.tf
@@ -1,49 +1,14 @@
-######################
-# Agent VM 0
-######################
-resource "azurerm_virtual_machine" "agent0" {
-  name                  = "${azurerm_resource_group.rg.name}-agent0"
-  location              = "${azurerm_resource_group.rg.location}"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
-  vm_size               = "${var.vmsize}"
-  network_interface_ids = ["${azurerm_network_interface.nic3.id}"]
+resource "azurerm_network_interface" "agent-nic" {
+  count = 100
 
-  connection {
-    type         = "ssh"
-    bastion_host = "${azurerm_public_ip.bastion_public_ip.fqdn}"
-    bastion_user = "${var.username}"
-    host         = "${element(azurerm_network_interface.nic3.*.private_ip_address, count.index)}"
-    user         = "${var.username}"
-    agent        = true
-  }
+  name                = "${azurerm_resource_group.rg.name}-nic${count.index}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
 
-  storage_image_reference {
-    publisher = "CoreOS"
-    offer     = "CoreOS"
-    sku       = "Stable"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.prefix}-${var.hostname}-osdisk3"
-    managed_disk_type = "StandardSSD_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-  }
-
-  os_profile {
-    computer_name  = "${var.prefix}-${var.hostname}-agent0"
-    admin_username = "${var.username}"
-    custom_data    = "${file(var.cloud_config_agent)}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/${var.username}/.ssh/authorized_keys"
-      key_data = "${file(var.public_key_path)}"
-    }
+  ip_configuration {
+    name                          = "${azurerm_resource_group.rg.name}-ipconfig"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "Dynamic"
   }
 
   tags {
@@ -52,20 +17,22 @@ resource "azurerm_virtual_machine" "agent0" {
 }
 
 ######################
-# Agent VM 1
+# Agent VM
 ######################
-resource "azurerm_virtual_machine" "agent1" {
-  name                  = "${azurerm_resource_group.rg.name}-agent1"
+resource "azurerm_virtual_machine" "agent" {
+  count = 100
+
+  name                  = "${azurerm_resource_group.rg.name}-agent${count.index}"
   location              = "${azurerm_resource_group.rg.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
   vm_size               = "${var.vmsize}"
-  network_interface_ids = ["${azurerm_network_interface.nic4.id}"]
+  network_interface_ids = ["${element(azurerm_network_interface.agent-nic.*.id, count.index)}"]
 
   connection {
     type         = "ssh"
     bastion_host = "${azurerm_public_ip.bastion_public_ip.fqdn}"
     bastion_user = "${var.username}"
-    host         = "${element(azurerm_network_interface.nic4.*.private_ip_address, count.index)}"
+    host         = "${element(azurerm_network_interface.agent-nic.*.0.private_ip_addresses, count.index)}"
     user         = "${var.username}"
     agent        = true
   }
@@ -78,67 +45,14 @@ resource "azurerm_virtual_machine" "agent1" {
   }
 
   storage_os_disk {
-    name              = "${var.prefix}-${var.hostname}-osdisk4"
+    name              = "${var.prefix}-${var.hostname}-osdisk${count.index}"
     managed_disk_type = "StandardSSD_LRS"
     caching           = "ReadWrite"
     create_option     = "FromImage"
   }
 
   os_profile {
-    computer_name  = "${var.prefix}-${var.hostname}-agent1"
-    admin_username = "${var.username}"
-    custom_data    = "${file(var.cloud_config_agent)}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/${var.username}/.ssh/authorized_keys"
-      key_data = "${file(var.public_key_path)}"
-    }
-  }
-
-  tags {
-    app = "mesos-cluster"
-  }
-}
-
-######################
-# Agent VM 2
-######################
-resource "azurerm_virtual_machine" "agent2" {
-  name                  = "${azurerm_resource_group.rg.name}-agent2"
-  location              = "${azurerm_resource_group.rg.location}"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
-  vm_size               = "${var.vmsize}"
-  network_interface_ids = ["${azurerm_network_interface.nic5.id}"]
-
-  connection {
-    type         = "ssh"
-    bastion_host = "${azurerm_public_ip.bastion_public_ip.fqdn}"
-    bastion_user = "${var.username}"
-    host         = "${element(azurerm_network_interface.nic5.*.private_ip_address, count.index)}"
-    user         = "${var.username}"
-    agent        = true
-  }
-
-  storage_image_reference {
-    publisher = "CoreOS"
-    offer     = "CoreOS"
-    sku       = "Stable"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.prefix}-${var.hostname}-osdisk5"
-    managed_disk_type = "StandardSSD_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-  }
-
-  os_profile {
-    computer_name  = "${var.prefix}-${var.hostname}-agent2"
+    computer_name  = "${var.prefix}-${var.hostname}-agent${count.index}"
     admin_username = "${var.username}"
     custom_data    = "${file(var.cloud_config_agent)}"
   }
