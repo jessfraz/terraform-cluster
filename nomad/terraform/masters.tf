f74e9b1150f5f4464209eb30e20ed95bb5690991
Jess Frazelle <acidburn@microsoft.com>
2018-10-14 04:21:11 -0400

    cleanup masters
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- nomad/terraform/masters.tf
+++ nomad/terraform/masters.tf
@@ -1,13 +1,13 @@
-resource "azurerm_network_interface" "master-nic0" {
-  count = "${var.master_count}"
+resource "azurerm_network_interface" "master-nic" {
+  count = "${var.master_count * length(var.locations)}"
 
-  name                = "${element(azurerm_resource_group.rg.*.name, 0)}-master-nic${count.index}"
-  location            = "${element(azurerm_resource_group.rg.*.location, 0)}"
-  resource_group_name = "${element(azurerm_resource_group.rg.*.name, 0)}"
+  name                = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}-master-nic${count.index}"
+  location            = "${element(azurerm_resource_group.rg.*.location, floor(count.index / var.master_count))}"
+  resource_group_name = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}"
 
   ip_configuration {
-    name                          = "${element(azurerm_resource_group.rg.*.name, 0)}-ipconfig"
-    subnet_id                     = "${element(azurerm_subnet.subnet.*.id, 0)}"
+    name                          = "ipconfig"
+    subnet_id                     = "${element(azurerm_subnet.subnet.*.id, floor(count.index / var.master_count))}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${cidrhost("10.0.0.0/16", count.index+5)}"
   }
@@ -21,20 +21,20 @@ resource "azurerm_network_interface" "master-nic0" {
 ######################
 # Master VM
 ######################
-resource "azurerm_virtual_machine" "master0" {
-  count = "${var.master_count}"
+resource "azurerm_virtual_machine" "master" {
+  count = "${var.master_count * length(var.locations)}"
 
-  name                  = "${element(azurerm_resource_group.rg.*.name, 0)}-master${count.index}"
-  location              = "${element(azurerm_resource_group.rg.*.location, 0)}"
-  resource_group_name   = "${element(azurerm_resource_group.rg.*.name, 0)}"
+  name                  = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}-master${count.index}"
+  location              = "${element(azurerm_resource_group.rg.*.location, floor(count.index / var.master_count))}"
+  resource_group_name   = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}"
   vm_size               = "${var.master_vmsize}"
-  network_interface_ids = ["${element(azurerm_network_interface.master-nic0.*.id, count.index)}"]
+  network_interface_ids = ["${element(azurerm_network_interface.master-nic.*.id, count.index)}"]
 
   connection {
     type         = "ssh"
     bastion_host = "${azurerm_public_ip.bastion_public_ip.fqdn}"
     bastion_user = "${var.username}"
-    host         = "${element(azurerm_network_interface.master-nic0.*.ip_configuration.0.private_ip_addresses, count.index)}"
+    host         = "${element(azurerm_network_interface.master-nic.*.ip_configuration.0.private_ip_addresses, count.index)}"
     user         = "${var.username}"
     agent        = true
   }
@@ -47,14 +47,14 @@ resource "azurerm_virtual_machine" "master0" {
   }
 
   storage_os_disk {
-    name              = "${element(azurerm_resource_group.rg.*.name, 0)}-master-osdisk${count.index}"
+    name              = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}-master-osdisk${count.index}"
     managed_disk_type = "StandardSSD_LRS"
     caching           = "ReadWrite"
     create_option     = "FromImage"
   }
 
   os_profile {
-    computer_name  = "${element(azurerm_resource_group.rg.*.name, 0)}-master${count.index}"
+    computer_name  = "${element(azurerm_resource_group.rg.*.name, floor(count.index / var.master_count))}-master${count.index}"
     admin_username = "${var.username}"
     custom_data    = "${file(var.cloud_config_master)}"
   }
